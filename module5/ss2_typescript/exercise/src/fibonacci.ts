5a450e890ade6292b4de94018ae3beb25389b577
nguyenvanhuy656688 <xpham9668@gmail.com>
2023-02-06 13:53:42 +0700

    first commit

--- module5/ss2_typescript/exercise/src/fibonacci.ts
+++ module5/ss2_typescript/exercise/src/fibonacci.ts
@@ -1,12 +1,12 @@
 
-function displayAndSumFibonacci(number: number) {
+function displayAndSumFibonacci(number: number):string {
     let fibonacciArr = [0, 1];
     let sum = 1;
     while (number > fibonacciArr.length) {
-        let c = 0;
-        c = fibonacciArr[fibonacciArr.length - 2] + fibonacciArr[fibonacciArr.length - 1];
-        sum += c;
-        fibonacciArr.push(c);
+        let cumulative = 0;
+        cumulative = fibonacciArr[fibonacciArr.length - 2] + fibonacciArr[fibonacciArr.length - 1];
+        sum += cumulative;
+        fibonacciArr.push(cumulative);
     }
     return  fibonacciArr.toString() + " Tổng là: " + sum;
 }
